2e065dd11da0b75f48431ca8e3531a607edee621
dEdmishka <m.melnyk.ia12@kpi.ua>
2023-10-18 11:28:22 +0300

    added cspell

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,4 +2,5 @@ module.exports = {
   '*': 'prettier --check --ignore-unknown',
   '*.js': 'eslint --cache',
   '*.{js,ts}': () => 'npm run typescript:check',
+  '*.{js,ts}': () => 'npm run cspell',
 };
