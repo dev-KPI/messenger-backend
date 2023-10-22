8730760ea24a6db5923c60a003e46a3f4a37c7a2
Melnyk Mykhailo <91133955+dEdmishka@users.noreply.github.com>
2023-10-22 21:17:46 +0300

    Add CSpell (#25)
    PR: https://github.com/dev-KPI/messenger-backend/pull/25

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,9 @@
+const toRelative = require('./src/utils/relativePath.js');
+
 module.exports = {
   '*': 'prettier --check --ignore-unknown',
   '*.js': 'eslint --cache',
   '*.{js,ts}': () => 'npm run typescript:check',
+  '*': (files) =>
+    `cspell --show-suggestions --quiet --gitignore ${toRelative(files)}`,
 };
---
+++ src/utils/relativePath.js
@@ -0,0 +1,6 @@
+const path = require('node:path');
+
+const toRelative = (files) =>
+  files.map((file) => path.relative(process.cwd(), file)).join(' ');
+
+module.exports = toRelative;
